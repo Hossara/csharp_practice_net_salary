87c8203901a57c6f293e9690776321b8fc69ca54
Hossara <hoseinaraghi84@gmail.com>
2022-11-22 07:43:23 +0330

    init

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharp_practice_net_salary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
